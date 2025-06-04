6711c1238e56294e8bb6a9829f36cc0b953cd745
dariaartyomova <144615378+dariaartyomova@users.noreply.github.com>
2025-06-04 23:51:59 +0200

    Add files via upload

---
+++ oop_labs/Laba_1/Laba_6/laba_6.py
@@ -0,0 +1,32 @@
+from v_keyboard import VirtualKeyboard
+
+if __name__ == "__main__":
+    keyboard = VirtualKeyboard()
+    
+    if not keyboard.load_state():
+        print( "No saved state found, using defaults")
+    else:
+        print("Keyboard state loaded")
+        print(keyboard.output)
+    
+    with open("data/keyboard_log.txt", "w") as log_file:
+        def print_and_log(message) -> None:
+            print(message)
+            print(keyboard.output)
+            log_file.write(message + "\n")
+        
+        print_and_log(keyboard.press_key("a"))
+        print_and_log(keyboard.press_key("b"))
+        print_and_log(keyboard.press_key("c"))
+        print_and_log(keyboard.press_key("undo"))
+        print_and_log(keyboard.press_key("undo"))
+        print_and_log(keyboard.press_key("redo"))
+        print_and_log(keyboard.press_key("ctrl++"))
+        print_and_log(keyboard.press_key("ctrl+-"))
+        print_and_log(keyboard.press_key("ctrl+p"))
+        print_and_log(keyboard.press_key("d"))
+        print_and_log(keyboard.press_key("undo"))
+        print_and_log(keyboard.press_key("undo"))
+        
+        keyboard.save_state()
+        print_and_log("Keyboard state saved")
