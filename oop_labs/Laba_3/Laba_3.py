7622bdd69601b00dccb06a125028aaf35a9529b7
dariaartyomova <144615378+dariaartyomova@users.noreply.github.com>
2025-06-11 22:07:57 +0200

    Update Laba_3.py

--- oop_labs/Laba_3/Laba_3.py
+++ oop_labs/Laba_3/Laba_3.py
@@ -19,10 +19,14 @@ class SimpleLogFilter(LogFilterProtocol):
 
 class ReLogFilter(LogFilterProtocol):
     def __init__(self, regex_pattern: str) -> None:
-        self.regex = re.compile(regex_pattern)
+        try:
+            self.regex = re.compile(regex_pattern)
+        except Exception as e:
+            print(f'regex error: {e}')
 
     def match(self, message: str) -> bool:
         return bool(self.regex.search(message))
+
 
 
 class LogHandlerProtocol(Protocol):
@@ -72,7 +76,7 @@ class Logger:
         self.filters = filters
 
     def write(self, message: str) -> None:
-        if any(filter_cls.match(message) for filter_cls in self.filters):
+        if all(filter_cls.match(message) for filter_cls in self.filters):
             for handler in self.handlers:
                 handler.handle(message)
 
@@ -85,11 +89,10 @@ if __name__ == '__main__':
     ]
 
     filters = [
-        SimpleLogFilter('IMPORTANT'),
-        SimpleLogFilter('INFO'),
+        SimpleLogFilter('404'),
+        SimpleLogFilter('ERROR'),
         ReLogFilter(r'(ERROR|WARNING) \d+')
     ]
-
     logger = Logger(handlers, filters)
 
     messages = [
